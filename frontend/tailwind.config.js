0feed6d21bf122ef35d0304539effbef2219149a
Yoichi0831 <2279137588@qq.com>
2025-02-13 14:41:30 +1100

    fixed vite including jsonwebtoken bug, now app is working

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // 如果你的根目录下有 index.html 文件
+    "./src/**/*.{js,jsx,ts,tsx}", // 扫描 src 目录下所有的 JS/TS/React 文件
+  ],
+  theme: {
+    extend: {
+      // 在这里可以扩展默认主题，比如增加自定义颜色、字体等等
+      colors: {
+        primary: "#1E3A8A",
+        secondary: "#9333EA",
+      },
+    },
+  },
+  plugins: [
+    // 如果需要安装插件，可以在这里引入，例如：
+    require('daisyui'),
+    // require('@tailwindcss/forms'),
+  ],
+};
