f0d2e293e7f3e9ebd8f063e086c9935272ca7154
Yoichi0831 <2279137588@qq.com>
2025-02-20 18:37:19 +1100

    ready for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,10 +7,14 @@ import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 // app.use((req, res, next) => {
 //     console.log(`Received ${req.method} request for ${req.url}`);
 //     next();
@@ -28,7 +32,12 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-
+if (process.env.NODE_ENV ==="production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 
 server.listen(PORT, () => {
